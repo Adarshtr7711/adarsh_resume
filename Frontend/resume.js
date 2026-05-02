c1a7a893b7396b8768bf05beb2a40d42e0089300
TRLP <adarsht7711@gmail.com>
2026-05-02 14:08:46 +0530

    Bharat Flag added

--- Frontend/resume.js
+++ Frontend/resume.js
@@ -1,10 +1,22 @@
 window.alert("Hii");
 
 
-var a = 10;
-document.write("a=");
-document.write(a);
+// var a = 10;
+// document.write("a=");
+// document.write(a);
 
-// Type convertion
-console.log(String(100));
-console.log(Number(6));
+// // Type convertion
+// console.log(String(100));
+// console.log(Number(6));
+
+// Ternary operator
+var num=4;
+if (num % 2===0)
+{
+    console.log("Even");
+}
+else
+{
+    console.log("Odd");
+}
+
